141d57a45ccb4dca9ec4a49f345e87d4418b1bd8
lujiangz <31911561+lujiangz@users.noreply.github.com>
2022-06-27 12:05:22 +0300

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msevuk <msevuk@student.42kocaeli.com.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 13:25:54 by msevuk            #+#    #+#             */
+/*   Updated: 2022/03/26 15:54:41 by msevuk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_remove_writed_line(char *line)
+{
+	int		counter;
+	int		counter_removed;
+	char	*new_removed_file;
+
+	counter = 0;
+	while (line[counter] && line[counter] != '\n')
+		counter++;
+	if (!line[counter])
+	{
+		free(line);
+		return (NULL);
+	}
+	new_removed_file = malloc(sizeof(char) * (ft_strlen(line) - counter + 1));
+	if (!new_removed_file)
+		return (NULL);
+	counter++;
+	counter_removed = 0;
+	while (line[counter])
+		new_removed_file[counter_removed++] = line[counter++];
+	new_removed_file[counter_removed] = '\0';
+	free(line);
+	return (new_removed_file);
+}
+
+char	*ft_write_readed_line(char *line)
+{
+	int		counter;
+	char	*new_writed_line;
+
+	counter = 0;
+	if (!line[counter])
+		return (NULL);
+	while (line[counter] != '\n' && line[counter] != '\0')
+		counter++;
+	new_writed_line = malloc(sizeof(char) * counter + 2);
+	if (!new_writed_line)
+		return (NULL);
+	counter = 0;
+	while (line[counter] && line[counter] != '\n')
+	{
+		new_writed_line[counter] = line[counter];
+		counter++;
+	}
+	if (line[counter] == '\n')
+	{
+		new_writed_line[counter] = line[counter];
+		counter++;
+	}
+	new_writed_line[counter] = '\0';
+	return (new_writed_line);
+}
+
+char	*ft_read_line(int fd, char *line)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(line, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		line = ft_strjoin(line, buff);
+	}
+	free(buff);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*write_readed_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = ft_read_line(fd, line);
+	if (!line)
+		return (NULL);
+	write_readed_line = ft_write_readed_line(line);
+	line = ft_remove_writed_line(line);
+	return (write_readed_line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msevuk <msevuk@student.42kocaeli.com.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 13:25:57 by msevuk            #+#    #+#             */
+/*   Updated: 2022/03/09 13:25:57 by msevuk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_line(int fd, char *line);
+char	*ft_write_readed_line(char *line);
+char	*ft_remove_writed_line(char *line);
+//UTILS
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
